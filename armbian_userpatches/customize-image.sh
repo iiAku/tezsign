c3d3b681e47dd30ba0ab007e4b09976e4b7f5f4c
v <cryi@tutanota.com>
2025-11-17 21:09:22 +0100

    move packages to packages_to_purge.txt

--- armbian_userpatches/customize-image.sh
+++ armbian_userpatches/customize-image.sh
@@ -2,13 +2,10 @@
 
 set -e
 
-apt autoremove \
-    media-types python3 wireguard-tools alsa-utils bash-completion armbian-config bc \
-    bluez bluez-firmware btrfs-progs cron curl debconf-utils debsums dialog dosfstools \
-    figlet fping  gpiod htop iputils-ping iw jq openssh-client wireless-regdb wpasupplicant \
-    wget ncurses-term toilet toilet-fonts bsdextrautils logrotate lsof man-db psmisc rsync \
-    whiptail locales libperl5.40 xkb-data libtirpc-common libtirpc3t64 libyaml-0-2 mtd-utils \
-    usbutils sensible-utils readline-common parted less ca-certificates apt-utils openssh-server \
-    openssh-sftp-server nano sudo passwd login login.defs sysvinit-utils udev
+# This command checks if the package status is NOT 'no such package' (i.e., it exists)
+# and then purges only the packages that are found.
+
+grep -Fvf <(dpkg-query -W -f='${Package}\n' | sed 's/^/#/') /tmp/overlay/packages_to_purge.txt | \
+    xargs sudo apt purge --assume-yes
 
 touch /root/.no_rootfs_resize
